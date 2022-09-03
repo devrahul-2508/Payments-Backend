18f1447697957b4f4f03aa45144abae803ea9a22
Rahul Roy <devrahul2508@gmail.com>
2022-09-03 08:55:38 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/paymentsDB");
+
+const paymentSchema = {
+    paymentID: String,
+    name: String,
+    amount: Number
+};
+
+const payment = mongoose.model("payments",paymentSchema);
+
+
+
+app.post("/payments",(req,res,next)=>{
+
+    console.log(req.body.paymentID);
+    console.log(req.body.name);
+    console.log(req.body.amount);
+
+    const newPayment = new payment({
+        paymentID: req.body.paymentID,
+        name: req.body.name,
+        amount: req.body.amount
+    })
+    newPayment.save((err)=>{
+        if(!err){
+            res.send("Successfully added payments to database");
+        }
+        else{
+            res.send("Error Occured");
+        }
+    })
+
+})
+
+app.listen(3000,(req,res)=>{
+    console.log("Server started at port 3000");
+})
